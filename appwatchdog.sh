31ab8e14190229368fe6049eed31895c1d4bd460
jeremykuzub <jeremy@jufaintermedia.com>
2024-05-18 00:52:25 +0000

    temp disable watchdogs

--- appwatchdog.sh
+++ appwatchdog.sh
@@ -4,7 +4,7 @@ ps auxw | grep timelapse_capture.py | grep -v grep > /dev/null
 if [ $? != 0 ]
 then
         echo "timelapse_capture process not runnning. Rebooting at $(date)" >> /home/pi/project558mini/logs/timelapse_capture.log
-        echo "warning reboot is disabled during devlopment"
+        echo "warning reboot is disabled during devlopment" >> /home/pi/project558mini/logs/timelpase_capture.log
         #sudo reboot now >> /home/pi/project558mini/logs/timelapse_capture.log
 else 
         echo "timelapse_capture.py is running"  >> /home/pi/project558mini/logs/timelapse_capture.log
--- bootloader.sh
+++ bootloader.sh
@@ -2,10 +2,12 @@
 
 #!/bin/bash
 
+APP_KILL="pkill -f -15 timelapse_capture.py"
 APP_ENTRY="cd /home/pi/project558mini;source ./env/bin/activate;python timelapse_capture.py"
 echo AurorEye Bootloader
 SHELL=/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin 
 cd "$(dirname "$0")"
+# eval $APP_KILL
 eval $APP_ENTRY
 
